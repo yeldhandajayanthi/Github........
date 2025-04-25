e829e2a264a583fc9f1411aaf8d773de5fedf9d8
yeldhandajayanthi <yeldhandajayanthi8@gmail.com>
2025-04-25 14:46:04 +0530

    changes done

--- factorial.py
+++ factorial.py
@@ -1,5 +1,5 @@
 num = int(input("Enter a number: "))
-fact = 1
+factorial = 1
 for i in range(1, num + 1):
-    fact *= i
-print("Factorial is:",fact)
+    factorial *= i
+print("Factorial is:",factorial)
